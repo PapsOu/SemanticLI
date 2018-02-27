f47d366fc307cd193488f878bb1e43973f5e3628
PapsOu <vivien.frasca@libre-informatique.fr>
2018-02-27 10:44:08 +0100

    Fix dropdown and form loading for dark theme

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Init dirs
-mkdir ./semantic-ui
-mkdir ./dist
-
 # Clone Semantic UI
 cd ./semantic-ui
 git clone --depth=1 https://github.com/Semantic-Org/Semantic-UI.git .
 cd ..
 
 echo "Semantic UI successfully cloned into project"
+
+# init npm packages
+npm install && npm install gulp
